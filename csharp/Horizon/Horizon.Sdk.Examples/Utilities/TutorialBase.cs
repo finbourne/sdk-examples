de0582ad4453015f09e47df3c56713b978400cad
dansalter1 <dan.salter@finbourne.com>
2023-11-27 11:25:23 +0000

    HZN-712 Added Horizon SDK examples (copied Lusid examples)

---
+++ csharp/Horizon/Horizon.Sdk.Examples/Utilities/TutorialBase.cs
@@ -0,0 +1,39 @@
+using Lusid.Sdk.Api;
+using Lusid.Sdk.Extensions;
+
+namespace Sdk.Examples.Utilities
+{
+    public class TutorialBase
+    {
+        internal readonly IApiFactory ApiFactory;
+        internal readonly ITransactionPortfoliosApi TransactionPortfoliosApi;
+        internal readonly IInstrumentsApi InstrumentsApi;
+        internal readonly IQuotesApi QuotesApi;
+        internal readonly IConfigurationRecipeApi RecipeApi;
+        internal readonly IPortfoliosApi PortfoliosApi;
+        internal readonly ICutLabelDefinitionsApi CutLabelDefinitionsApi;
+        internal readonly IOrdersApi OrdersApi;
+        internal readonly ICorporateActionSourcesApi CorporateActionSourcesApi;
+        internal readonly IStructuredResultDataApi StructuredResultDataApi;
+        internal readonly IPropertyDefinitionsApi PropertyDefinitionsApi;
+
+        protected TutorialBase()
+        {
+            // Initialize all the API end points
+            ApiFactory = TestLusidApiFactoryBuilder.CreateApiFactory("secrets.json");
+            PortfoliosApi = ApiFactory.Api<IPortfoliosApi>();
+            TransactionPortfoliosApi = ApiFactory.Api<ITransactionPortfoliosApi>();
+            InstrumentsApi = ApiFactory.Api<IInstrumentsApi>();
+            QuotesApi = ApiFactory.Api<IQuotesApi>();
+            ApiFactory.Api<IComplexMarketDataApi>();
+            RecipeApi = ApiFactory.Api<IConfigurationRecipeApi>();
+            ApiFactory.Api<IAggregationApi>();
+            CutLabelDefinitionsApi = ApiFactory.Api<CutLabelDefinitionsApi>();
+            OrdersApi = ApiFactory.Api<IOrdersApi>();
+            CorporateActionSourcesApi = ApiFactory.Api<ICorporateActionSourcesApi>();
+            ApiFactory.Api<IConventionsApi>();
+            StructuredResultDataApi = ApiFactory.Api<IStructuredResultDataApi>();
+            PropertyDefinitionsApi = ApiFactory.Api<IPropertyDefinitionsApi>();
+        }
+    }
+}
