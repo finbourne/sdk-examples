3ba6243d0b722c88b5e9a76b363c7afc40a976a3
dansalter1 <dan.salter@finbourne.com>
2023-12-06 11:05:47 +0000

    HZN-712 Instrument tutorial formatting changes

--- csharp/Sdk.Examples/Horizon/Tutorials/Instrument/Instrument.cs
+++ csharp/Sdk.Examples/Horizon/Tutorials/Instrument/Instrument.cs
@@ -20,13 +20,11 @@ namespace Sdk.Examples.Horizon.Tutorials.Instrument
         public async Task Search_OpenFigi_Market_Sector()
         {
             // Check Equity is a valid Market Sector 
-            var allowedMarketSectors =
-                await InstrumentApi.GetOpenFigiParameterOptionAsync(OpenFigiParameterOptionName.MarketSector);
+            var allowedMarketSectors = await InstrumentApi.GetOpenFigiParameterOptionAsync(OpenFigiParameterOptionName.MarketSector);
             Assert.That(allowedMarketSectors, Contains.Item(new AllowedParameterValue("Equity")));
 
             // Query OpenFigi with Market Sector filter
-            var openFigiMarketSectorResult =
-                await InstrumentApi.SearchOpenFigiAsync("VOD", false, 25, "Equity");
+            var openFigiMarketSectorResult = await InstrumentApi.SearchOpenFigiAsync("VOD", false, 25, "Equity");
             Assert.That(openFigiMarketSectorResult.Results, Is.Not.Empty);
         }
 
@@ -34,22 +32,19 @@ namespace Sdk.Examples.Horizon.Tutorials.Instrument
         public async Task Search_OpenFigi_PermId()
         {
             // Query OpenFigi and enrich with PermId data
-            var openFigiPermIdResult =
-                await InstrumentApi.SearchOpenFigiAsync("VOD", true);
+            var openFigiPermIdResult = await InstrumentApi.SearchOpenFigiAsync("VOD", true);
             Assert.That(openFigiPermIdResult.Results, Is.Not.Empty);
         }
         
         [Test]
         public async Task Vendors(){
             // Check Common Stock is a valid Security Type
-            var allowedSecurityType =
-                await InstrumentApi.GetOpenFigiParameterOptionAsync(OpenFigiParameterOptionName.SecurityType);
+            var allowedSecurityType = await InstrumentApi.GetOpenFigiParameterOptionAsync(OpenFigiParameterOptionName.SecurityType);
             Assert.That(allowedSecurityType.Contains(new AllowedParameterValue("Common Stock")));
 
             // Get VendorProducts of any supported and licensed integrations
             // filtered by Market Sector and Security Type
-            var vendorsResult = 
-                await InstrumentApi.VendorsAsync("Equity", "Common Stock");
+            var vendorsResult = await InstrumentApi.VendorsAsync("Equity", "Common Stock");
             Assert.That(vendorsResult, Has.One.Matches<VendorProduct>(v => v.VendorName == "Bloomberg"));
             Assert.That(vendorsResult, Has.One.Matches<VendorProduct>(v => v.VendorName == "SIX"));
         }
