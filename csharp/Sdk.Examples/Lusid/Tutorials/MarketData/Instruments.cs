681a7cb8b964c520cbe535b078ae27820181205a
Matt-Storey-Finbourne <165797893+Matt-Storey-Finbourne@users.noreply.github.com>
2024-07-29 16:20:01 +0100

    CTOOLS-290: Additional logging (#12)

--- csharp/Sdk.Examples/Lusid/Tutorials/MarketData/Instruments.cs
+++ csharp/Sdk.Examples/Lusid/Tutorials/MarketData/Instruments.cs
@@ -5,6 +5,7 @@ using Lusid.Sdk.Client;
 using Lusid.Sdk.Model;
 using NUnit.Framework;
 using Sdk.Examples.Lusid.Utilities;
+using System;
 
 namespace Sdk.Examples.Lusid.Tutorials.MarketData
 {
@@ -30,90 +31,97 @@ namespace Sdk.Examples.Lusid.Tutorials.MarketData
 
         private void SeedInstrumentMaster()
         {
-            var upsertInstrumentsResponse = InstrumentsApi.UpsertInstruments(new Dictionary<string, InstrumentDefinition>
+            try {
+                var upsertInstrumentsResponse = InstrumentsApi.UpsertInstruments(new Dictionary<string, InstrumentDefinition>
+                {
+                    ["correlationId1"] = new InstrumentDefinition(
+                        name: "VODAFONE GROUP PLC",
+
+                        /*
+                            Instruments are created with a set of identifiers
+                            each under a different scheme
+                        */
+                        identifiers: new Dictionary<string, InstrumentIdValue>
+                        {
+                            [FigiScheme] = new InstrumentIdValue("BBG000C6K6G9"),
+                            [CustomInternalScheme] = new InstrumentIdValue("INTERNAL_ID_1"),
+                            [IsinScheme] = new InstrumentIdValue("GB00BH4HKS39"),
+                            [SedolScheme] = new InstrumentIdValue("BH4HKS3")
+                        }
+                    ),
+
+                    ["correlationId2"] = new InstrumentDefinition(
+                        name: "BARCLAYS PLC",
+
+                        /*
+                            Instruments are created with a set of identifiers
+                            each under a different scheme
+                        */
+                        identifiers: new Dictionary<string, InstrumentIdValue>
+                        {
+                            [FigiScheme] = new InstrumentIdValue("BBG000C04D57"),
+                            [CustomInternalScheme] = new InstrumentIdValue("INTERNAL_ID_2"),
+                            [IsinScheme] = new InstrumentIdValue("GB0031348658"),
+                            [SedolScheme] = new InstrumentIdValue("3134865")
+                        }
+                    ),
+
+                    ["correlationId3"] = new InstrumentDefinition(
+                        name: "NATIONAL GRID PLC",
+
+                        /*
+                            Instruments are created with a set of identifiers
+                            each under a different scheme
+                        */
+                        identifiers: new Dictionary<string, InstrumentIdValue>
+                        {
+                            [FigiScheme] = new InstrumentIdValue("BBG000FV67Q4"),
+                            [CustomInternalScheme] = new InstrumentIdValue("INTERNAL_ID_3"),
+                            [IsinScheme] = new InstrumentIdValue("GB00BDR05C01"),
+                            [SedolScheme] = new InstrumentIdValue("BDR05C0")
+                        }
+                    ),
+
+                    ["correlationId4"] = new InstrumentDefinition(
+                        name: "SAINSBURY (J) PLC",
+
+                        /*
+                            Instruments are created with a set of identifiers
+                            each under a different scheme
+                        */
+                        identifiers: new Dictionary<string, InstrumentIdValue>
+                        {
+                            [FigiScheme] = new InstrumentIdValue("BBG000BF0KW3"),
+                            [CustomInternalScheme] = new InstrumentIdValue("INTERNAL_ID_4"),
+                            [IsinScheme] = new InstrumentIdValue("GB00B019KW72"),
+                            [SedolScheme] = new InstrumentIdValue("B019KW7")
+                        }
+                    ),
+
+                    ["correlationId5"] = new InstrumentDefinition(
+                        name: "TAYLOR WIMPEY PLC",
+
+                        /*
+                            Instruments are created with a set of identifiers
+                            each under a different scheme
+                        */
+                        identifiers: new Dictionary<string, InstrumentIdValue>
+                        {
+                            [FigiScheme] = new InstrumentIdValue("BBG000BF4KL1"),
+                            [CustomInternalScheme] = new InstrumentIdValue("INTERNAL_ID_5"),
+                            [IsinScheme] = new InstrumentIdValue("GB0008782301"),
+                            [SedolScheme] = new InstrumentIdValue("0878230")
+                        }
+                    )
+                });
+
+                Assert.That(upsertInstrumentsResponse.Values, Has.Count.EqualTo(5));
+            }
+            catch(Exception ex)
             {
-                ["correlationId1"] = new InstrumentDefinition(
-                    name: "VODAFONE GROUP PLC",
-
-                    /*
-                        Instruments are created with a set of identifiers
-                        each under a different scheme
-                     */
-                    identifiers: new Dictionary<string, InstrumentIdValue>
-                    {
-                        [FigiScheme] = new InstrumentIdValue("BBG000C6K6G9"),
-                        [CustomInternalScheme] = new InstrumentIdValue("INTERNAL_ID_1"),
-                        [IsinScheme] = new InstrumentIdValue("GB00BH4HKS39"),
-                        [SedolScheme] = new InstrumentIdValue("BH4HKS3")
-                    }
-                 ),
-
-                ["correlationId2"] = new InstrumentDefinition(
-                    name: "BARCLAYS PLC",
-
-                    /*
-                        Instruments are created with a set of identifiers
-                        each under a different scheme
-                     */
-                    identifiers: new Dictionary<string, InstrumentIdValue>
-                    {
-                        [FigiScheme] = new InstrumentIdValue("BBG000C04D57"),
-                        [CustomInternalScheme] = new InstrumentIdValue("INTERNAL_ID_2"),
-                        [IsinScheme] = new InstrumentIdValue("GB0031348658"),
-                        [SedolScheme] = new InstrumentIdValue("3134865")
-                    }
-                ),
-
-                ["correlationId3"] = new InstrumentDefinition(
-                    name: "NATIONAL GRID PLC",
-
-                    /*
-                        Instruments are created with a set of identifiers
-                        each under a different scheme
-                     */
-                    identifiers: new Dictionary<string, InstrumentIdValue>
-                    {
-                        [FigiScheme] = new InstrumentIdValue("BBG000FV67Q4"),
-                        [CustomInternalScheme] = new InstrumentIdValue("INTERNAL_ID_3"),
-                        [IsinScheme] = new InstrumentIdValue("GB00BDR05C01"),
-                        [SedolScheme] = new InstrumentIdValue("BDR05C0")
-                    }
-                ),
-
-                ["correlationId4"] = new InstrumentDefinition(
-                    name: "SAINSBURY (J) PLC",
-
-                    /*
-                        Instruments are created with a set of identifiers
-                        each under a different scheme
-                     */
-                    identifiers: new Dictionary<string, InstrumentIdValue>
-                    {
-                        [FigiScheme] = new InstrumentIdValue("BBG000BF0KW3"),
-                        [CustomInternalScheme] = new InstrumentIdValue("INTERNAL_ID_4"),
-                        [IsinScheme] = new InstrumentIdValue("GB00B019KW72"),
-                        [SedolScheme] = new InstrumentIdValue("B019KW7")
-                    }
-                ),
-
-                ["correlationId5"] = new InstrumentDefinition(
-                    name: "TAYLOR WIMPEY PLC",
-
-                    /*
-                        Instruments are created with a set of identifiers
-                        each under a different scheme
-                     */
-                    identifiers: new Dictionary<string, InstrumentIdValue>
-                    {
-                        [FigiScheme] = new InstrumentIdValue("BBG000BF4KL1"),
-                        [CustomInternalScheme] = new InstrumentIdValue("INTERNAL_ID_5"),
-                        [IsinScheme] = new InstrumentIdValue("GB0008782301"),
-                        [SedolScheme] = new InstrumentIdValue("0878230")
-                    }
-                )
-            });
-
-            Assert.That(upsertInstrumentsResponse.Values, Has.Count.EqualTo(5));
+                Console.WriteLine($"An unexpected error occurred: {ex.Message}");
+                throw;
+            }
         }
 
 
